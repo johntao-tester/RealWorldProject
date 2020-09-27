34951806eddeef3f968edee5c5cbb975c351465b
Johnny <1johntao1@gmail.com>
2020-09-26 23:44:58 -0400

    Messi

--- ear/src/main/java/WhiteBoard.java
+++ ear/src/main/java/WhiteBoard.java
@@ -62,7 +62,7 @@ public class WhiteBoard {
         System.out.println(fibSeq(fibSer));
         System.out.println("Bla bla bla");
         System.out.println("Ha ha ha");
-        System.out.println("Im a truck driver tram tram tram");
+        System.out.println("Im a truck driver");
     }
 
     public static int fibonacciRecurcive(int a) {
